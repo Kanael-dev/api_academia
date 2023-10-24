9735746023c5e9a13b39a9abc51c29cbfc500a98
Kanael-dev <kanael.dev@gmail.com>
2023-10-24 14:16:04 -0400

    Ajuste conexao

--- index.js
+++ index.js
@@ -10,41 +10,31 @@ mongoose.set("strictQuery", false);
 
 app.use(cors());
 
- 
-// Variveis de ambiente
+// Variáveis de ambiente
 const userNameMongo = process.env.USER_NAME;
 const userPasswordMongo = process.env.USER_PASSWORD;
+const dbName = 'mydb'; // Nome do banco de dados
 
-//Configuração para ler o JSON //
-app.use(
-  express.urlencoded({
-    extended: true,
-  })
-);
-
+// Configuração para ler o JSON
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-// Validador
 
-const router = express.Router();
-
-
-//Rotas api //
+// Rotas api
 const userRouter = require("./backend/router/router_usuarios");
 const userPlanos = require("./backend/router/router_planos");
 const userExercicios = require("./backend/router/router_exercicios");
-//const auth = require("./backend/router/recurso-protegido");
-
 
 app.use("/usuarios", userRouter);
 app.use("/planos", userPlanos)
 app.use("/exercicios", userExercicios)
-//app.use("/auth", auth)
-//Configuração para ler o JSON //
 
-mongoose.connect(
-  `mongodb+srv://${userNameMongo}:${userPasswordMongo}@kanael0.yuazcu9.mongodb.net/`
-)
+// Configuração para ler o JSON
+
+mongoose.connect(`mongodb+srv://${userNameMongo}:${userPasswordMongo}@kanael0.yuazcu9.mongodb.net/${dbName}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 .then(() => {
   console.log("Conectado com sucesso");
   app.listen(3001);
